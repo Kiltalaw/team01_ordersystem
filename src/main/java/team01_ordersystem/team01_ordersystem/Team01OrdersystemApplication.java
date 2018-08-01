9b6e1ed539b089daef4afb6cf9cd1d321bc79ab3
jiez <526013323@qq.com>
2018-08-01 16:43:40 +0800

    create:project

---
+++ src/main/java/team01_ordersystem/team01_ordersystem/Team01OrdersystemApplication.java
@@ -0,0 +1,12 @@
+package team01_ordersystem.team01_ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team01OrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team01OrdersystemApplication.class, args);
+    }
+}
